0ac14ec0a02e70315ec3a1d1af22861ce364286f
mrkhoa1996 <mrkhoa1996@gmail.com>
2019-08-31 11:05:05 +1000

    subject insert modal

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactModal from 'react-modal';
 import './MainScreen.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -13,11 +14,19 @@ function Subject(props) {
     );
 }
 
-function Model(props) {
-
+function Modal(props) {
+    return (
+        <ReactModal />
+    );
 }
 
 function MainScreen(props) {
+    state = {};
+    
+    const createSubject = () => {
+
+    };
+
     return (
         <div className="main-screen">
             <header>Welcome to My App</header>
@@ -26,10 +35,10 @@ function MainScreen(props) {
             <Subject name="C++" numCards="100" />
             <Subject name="Algorithm" numCards="200" />
             <Subject name="Business Management" numCards="50" />
-            <div className="add-section" onClick="">
+            <div className="add-section" onClick={createSubject}>
                 <FontAwesomeIcon icon={faPlus}/> Add more subjects...
             </div>
-
+            <Modal /> 
         </div>
     );
 }
